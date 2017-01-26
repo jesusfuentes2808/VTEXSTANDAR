e4ef87df9657a76876ff6261e3bee2fac821708e
Jesus Fuentes <jesus.fuentes@lideratis.com>
2017-01-26 11:41:45 -0500

    Se agrego la validación de SUFIJOS en GlobalVar

--- GlobalVar.js
+++ GlobalVar.js
@@ -1,6 +1,7 @@
 /******************************************************************************************************************/
 // Variables Globales
 /******************************************************************************************************************/
+window.clientID    = window.cm_ClientID.split("|")[1];
 window.GLCAT       = "CAT: ";
 window.GLPROD      = "PROD: ";
 window.GLART       = "ART: ";
@@ -8,3 +9,13 @@ window.GLSEPARADOR = " | ";
 window.BUSCADORCRE = "Búsqueda con Resultados";
 window.BUSCADORSRE = "Búsqueda sin Resultados";
 window.INICIO      = "Portada";
+
+/******************************************************************************************************************/
+// Validacin por SiteID
+/******************************************************************************************************************/
+     
+if(window.clientID=="WNF"){
+	window.SUFIJO = "WONG";
+}else if(window.clientID=="JMB"){
+	window.SUFIJO = "JUMBO";
+}
