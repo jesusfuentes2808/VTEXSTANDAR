2d72514f2909b3134f7ba878020269145a7eed2a
Jesus Fuentes <jesus.fuentes@lideratis.com>
2017-01-26 11:18:43 -0500

    Se agregaron variables Globales para el uso en la standarización

---
+++ GlobalVar.js
@@ -0,0 +1,10 @@
+/******************************************************************************************************************/
+// Variables Globales
+/******************************************************************************************************************/
+window.GLCAT       = "CAT: ";
+window.GLPROD      = "PROD: ";
+window.GLART       = "ART: ";
+window.GLSEPARADOR = " | ";
+window.BUSCADORCRE = "Búsqueda con Resultados";
+window.BUSCADORSRE = "Búsqueda sin Resultados";
+window.INICIO      = "Portada";
